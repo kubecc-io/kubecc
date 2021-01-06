4a7807cd6386674c1f0742d3a7a00ac5d29ddb63
Joe Kralicky <joekralicky@gmail.com>
2021-01-05 21:18:05 -0500

    Update mgr to use controller-runtime client

--- mgr/main.go
+++ mgr/main.go
@@ -3,21 +3,22 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
-	"io/ioutil"
 	"net"
-	"strings"
 
 	"github.com/cobalt77/kube-distcc/mgr/api"
 	kdcv1alpha1 "github.com/cobalt77/kube-distcc/operator/api/v1alpha1"
 	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
+	traefikv1alpha1 "github.com/traefik/traefik/v2/pkg/provider/kubernetes/crd/traefik/v1alpha1"
 	"google.golang.org/grpc"
 	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/kubernetes"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type server struct {
@@ -32,58 +33,64 @@ var (
 	}
 )
 
-func namespace() string {
-	if data, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace"); err == nil {
-		if ns := strings.TrimSpace(string(data)); len(ns) > 0 {
-			return ns
-		}
-	}
+var (
+	scheme = runtime.NewScheme()
+	config *rest.Config
+)
 
-	return "default"
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(kdcv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(traefikv1alpha1.AddToScheme(scheme))
+	// +kubebuilder:scaffold:scheme
 }
-
-var client *kubernetes.Clientset
 
 func (s *server) Status(ctx context.Context, _ *api.StatusRequest) (*api.StatusResponse, error) {
 	log.Info("=> Handling status request")
 
-	ns := namespace()
+	cl, err := client.New(config, client.Options{Scheme: scheme})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	distccs := &kdcv1alpha1.DistccList{}
-	err := client.
-		RESTClient().
-		Get().
-		Resource("distccs"). // todo: this doesnt work
-		Do(ctx).
-		Into(distccs)
-
+	err = cl.List(ctx, distccs, &client.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
 
-	distcc := distccs.Items[0]
-
-	svcList, err := client.CoreV1().Services(ns).List(ctx, metav1.ListOptions{
-		LabelSelector: fmt.Sprintf("kdistcc.io/distcc_cr=%s", distcc.Name),
-	})
-
-	if err != nil {
-		return nil, err
+	if len(distccs.Items) == 0 {
+		log.Warning("No distccs found in the cluster")
+		return &api.StatusResponse{Agents: []*api.Agent{}}, nil
 	}
 
 	response := &api.StatusResponse{
 		Agents: []*api.Agent{},
 	}
 
-	for _, svc := range svcList.Items {
-		if svc.Spec.Type != v1.ServiceTypeExternalName {
-			return nil, errors.New(
-				"The distcc server appears to be improperly configured (wrong service type)")
-		}
-		response.Agents = append(response.Agents, &api.Agent{
-			Address: svc.Spec.ExternalName,
-			Port:    443,
+	for _, distcc := range distccs.Items {
+		svcList := &v1.ServiceList{}
+		err = cl.List(ctx, svcList, &client.ListOptions{
+			LabelSelector: labels.SelectorFromSet(labels.Set{
+				"kdistcc.io/distcc_cr": distcc.Name,
+			}),
 		})
+
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+
+		for _, svc := range svcList.Items {
+			if svc.Spec.Type != v1.ServiceTypeExternalName {
+				return nil, errors.New(
+					"The distcc server appears to be improperly configured (wrong service type)")
+			}
+			response.Agents = append(response.Agents, &api.Agent{
+				Address: svc.Spec.ExternalName,
+				Port:    443,
+			})
+		}
 	}
 
 	return response, nil
@@ -92,13 +99,13 @@ func (s *server) Status(ctx context.Context, _ *api.StatusRequest) (*api.StatusR
 func main() {
 	log.SetLevel(logrus.DebugLevel)
 	log.Info("Server starting")
-	config, err := rest.InClusterConfig()
+	cfg, err := rest.InClusterConfig()
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	client = kubernetes.NewForConfigOrDie(config)
+	config = cfg
 
 	listener, err := net.Listen("tcp", ":9090")
 	if err != nil {
