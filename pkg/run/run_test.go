80917bd1083bbbfb9249bb1486d0af30f6503409
Joe Kralicky <joekralicky@gmail.com>
2021-02-28 22:20:27 -0500

    Executor tests work in progress
    Co-authored-by: Jake <dragozir@users.noreply.github.com>

---
+++ pkg/run/run_test.go
@@ -0,0 +1,34 @@
+package run_test
+
+import (
+	"github.com/cobalt77/kubecc/internal/logkc"
+	. "github.com/cobalt77/kubecc/internal/testutil"
+	"github.com/cobalt77/kubecc/pkg/identity"
+	"github.com/cobalt77/kubecc/pkg/meta"
+	"github.com/cobalt77/kubecc/pkg/run"
+	"github.com/cobalt77/kubecc/pkg/tracing"
+	"github.com/cobalt77/kubecc/pkg/types"
+
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Run", func() {
+	ctx := meta.NewContext(
+		meta.WithProvider(identity.Component, meta.WithValue(types.TestComponent)),
+		meta.WithProvider(identity.UUID),
+		meta.WithProvider(logkc.Logger),
+		meta.WithProvider(tracing.Tracer),
+	)
+	// lg := meta.Log(ctx)
+
+	When("something", func() {
+		It("should do something", func() {
+			exec := run.NewQueuedExecutor(run.WithUsageLimits(&types.UsageLimits{
+				ConcurrentProcessLimit:  1,
+				QueuePressureMultiplier: 1,
+				QueueRejectMultiplier:   1,
+			}))
+			exec.Exec(run.Begin(ctx, &SleepRunner{Duration: 1}, nil))
+		})
+	})
+})
