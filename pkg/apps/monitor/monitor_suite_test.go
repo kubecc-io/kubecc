894c6d44de35abba08c1f84334a3cd3e5715b9db
cobalt77 <8194899+cobalt77@users.noreply.github.com>
2021-03-02 12:50:22 -0500

    Convert listener to use stream manager

--- pkg/apps/monitor/monitor_suite_test.go
+++ pkg/apps/monitor/monitor_suite_test.go
@@ -10,8 +10,8 @@ import (
 
 func TestMonitor(t *testing.T) {
 	RegisterFailHandler(Fail)
-	// SetDefaultEventuallyTimeout(3 * time.Second)
-	SetDefaultEventuallyTimeout(1 * time.Hour)
+	SetDefaultEventuallyTimeout(3 * time.Second)
+	// SetDefaultEventuallyTimeout(1 * time.Hour)
 	SetDefaultEventuallyPollingInterval(50 * time.Millisecond)
 	RunSpecs(t, "Monitor Suite")
 }
