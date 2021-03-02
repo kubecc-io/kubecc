27c66879793695688f46eca07215d5ac558bbbe3
Joe Kralicky <joekralicky@gmail.com>
2021-03-01 19:32:57 -0500

    Merge agent, consumerd, monitor, scheduler into single binary

---
+++ cmd/kubecc/components/consumerd/consumerd.go
@@ -0,0 +1,78 @@
+package commands
+
+import (
+	"net"
+
+	"github.com/cobalt77/kubecc/internal/logkc"
+	"github.com/cobalt77/kubecc/pkg/apps/consumerd"
+	cctoolchain "github.com/cobalt77/kubecc/pkg/cc/toolchain"
+	"github.com/cobalt77/kubecc/pkg/config"
+	"github.com/cobalt77/kubecc/pkg/host"
+	"github.com/cobalt77/kubecc/pkg/identity"
+	"github.com/cobalt77/kubecc/pkg/meta"
+	"github.com/cobalt77/kubecc/pkg/servers"
+	"github.com/cobalt77/kubecc/pkg/tracing"
+	"github.com/cobalt77/kubecc/pkg/types"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+func run(cmd *cobra.Command, args []string) {
+	ctx := meta.NewContext(
+		meta.WithProvider(identity.Component, meta.WithValue(types.Consumerd)),
+		meta.WithProvider(identity.UUID),
+		meta.WithProvider(logkc.Logger),
+		meta.WithProvider(tracing.Tracer),
+		meta.WithProvider(host.SystemInfo),
+	)
+	lg := meta.Log(ctx)
+
+	conf := (&config.ConfigMapProvider{}).Load(ctx, types.Consumerd).Consumerd
+
+	schedulerCC, err := servers.Dial(ctx, conf.SchedulerAddress,
+		servers.WithTLS(true),
+	)
+	if err != nil {
+		lg.With(zap.Error(err)).Fatal("Error dialing scheduler")
+	}
+
+	lg.With("address", schedulerCC.Target()).Info("Dialing scheduler")
+	schedulerClient := types.NewSchedulerClient(schedulerCC)
+
+	d := consumerd.NewConsumerdServer(ctx,
+		consumerd.WithUsageLimits(&types.UsageLimits{
+			ConcurrentProcessLimit:  int32(conf.UsageLimits.ConcurrentProcessLimit),
+			QueuePressureMultiplier: conf.UsageLimits.QueuePressureMultiplier,
+			QueueRejectMultiplier:   conf.UsageLimits.QueueRejectMultiplier,
+		}),
+		consumerd.WithToolchainRunners(cctoolchain.AddToStore),
+		consumerd.WithSchedulerClient(schedulerClient, schedulerCC),
+	)
+
+	go d.RunSchedulerClient()
+
+	listener, err := net.Listen("tcp", conf.ListenAddress)
+	if err != nil {
+		lg.With(
+			zap.Error(err),
+			zap.String("address", conf.ListenAddress),
+		).Fatal("Error listening on socket")
+	}
+	lg.With("addr", listener.Addr()).Info("Listening")
+	if err != nil {
+		lg.With(zap.Error(err)).Fatal("Could not start consumerd")
+	}
+
+	srv := servers.NewServer(ctx)
+	types.RegisterConsumerdServer(srv, d)
+	err = srv.Serve(listener)
+	if err != nil {
+		lg.Error(err.Error())
+	}
+}
+
+var Command = &cobra.Command{
+	Use:   "consumerd",
+	Short: "Run the consumerd service",
+	Run:   run,
+}
--- cmd/kubecc/components/monitor/monitor.go
+++ cmd/kubecc/components/monitor/monitor.go
@@ -1,21 +1,20 @@
-package main
+package commands
 
 import (
 	"net"
 
 	"github.com/cobalt77/kubecc/internal/logkc"
 	"github.com/cobalt77/kubecc/pkg/apps/monitor"
+	"github.com/cobalt77/kubecc/pkg/config"
 	"github.com/cobalt77/kubecc/pkg/identity"
 	"github.com/cobalt77/kubecc/pkg/meta"
 	"github.com/cobalt77/kubecc/pkg/servers"
 	"github.com/cobalt77/kubecc/pkg/tracing"
 	"github.com/cobalt77/kubecc/pkg/types"
-	"go.uber.org/zap"
+	"github.com/spf13/cobra"
 )
 
-var lg *zap.SugaredLogger
-
-func main() {
+func run(cmd *cobra.Command, args []string) {
 	ctx := meta.NewContext(
 		meta.WithProvider(identity.Component, meta.WithValue(types.Monitor)),
 		meta.WithProvider(identity.UUID),
@@ -24,13 +23,15 @@ func main() {
 	)
 	lg := meta.Log(ctx)
 
-	extListener, err := net.Listen("tcp", ":9090")
+	conf := (&config.ConfigMapProvider{}).Load(ctx, types.Consumerd).Monitor
+
+	extListener, err := net.Listen("tcp", conf.ListenAddress.External)
 	if err != nil {
 		panic(err.Error())
 	}
 	lg.With("addr", extListener.Addr().String()).Info("External API listening")
 
-	intListener, err := net.Listen("tcp", ":9091")
+	intListener, err := net.Listen("tcp", conf.ListenAddress.Internal)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -52,4 +53,10 @@ func main() {
 	if err != nil {
 		lg.Error(err)
 	}
+}
+
+var Command = &cobra.Command{
+	Use:   "monitor",
+	Short: "Run the monitor service",
+	Run:   run,
 }
